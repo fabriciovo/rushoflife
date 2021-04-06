9a02085d0d7775b35d291feff081661630e973d0
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2021-04-06 17:31:16 -0300

    Modified classes
    Modified GameController class
    Modified MiniGameEntity class

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -5,12 +5,12 @@ class_name GameController
 const _MiniGame = preload("res://Assets/MiniGameEntity.tres")
 const _Player = preload("res://Assets/Player.tres")
 
-onready var Level = 0 setget set_Level 
-onready var Stage = 0 setget set_Stage
-onready var Score = 0
+export var Level = 0 setget set_Level
+export var Stage = 0 setget set_Stage
+export var Score = 0
 
 func set_Level(level):
 	Level =  level
-	
+
 func set_Stage(stage):
 	Stage =  stage
--- Scripts/MiniGameEntity.gd
+++ Scripts/MiniGameEntity.gd
@@ -2,7 +2,13 @@ extends Resource
 
 class_name MiniGameEntity
 
-export(String) var Name = ""
-export(int)var MiniGameTimer = 5
-export(int)var Dificult = 1
-export(int)var Type = 0
+#export(String) var Name = ""
+export var MiniGameTimer = 5.0 setget set_Timer
+export var Dificult = 1 setget set_Dificult
+export var Type = 0
+
+func set_Timer(timer):
+	MiniGameTimer = timer
+
+func set_Dificult(dificult):
+	Dificult = dificult
