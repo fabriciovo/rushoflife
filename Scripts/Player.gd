6dcbadba8e60fa15ad1c70af8ed1d90547f12543
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2021-04-12 17:13:33 -0300

    Adjusted Player entity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,11 @@
 extends Resource
 
-
 class_name Player
 
 export(String) var Name = ""
 export(int)var Money = 0
 export(int)var Life = 3
 export(int)var Sanity = 0
+
+func set_Name(name):
+	Name = name
