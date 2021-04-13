d0b843a9be45e8928ed44fa82132efac5d901a75
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2021-04-13 20:48:25 -0300

    Game Menu Update
    Updated game menu design adding images and changing buttons

--- Scripts/GameMenu.gd
+++ Scripts/GameMenu.gd
@@ -2,21 +2,18 @@ extends Node2D
 
 const GameController = preload("res://Assets/GameController.tres")
 var minigame = preload("res://Scenes/MiniGame.tscn")
-	
-func _on_Next_pressed():
-	pass # Replace with function body.
 
-func _on_Back_pressed():
-	pass # Replace with function body.
+onready var scoreBoard = $Screen/ScoreBoard
+
+func _ready():
+	GameController.Level = 0
+	scoreBoard.text = "Score: " + str(GameController.Score) + '\n' + GameController._Player.Name + '\n' + str(GameController.Stage)
 
 func _on_StartMiniGame_pressed():
 	GameController.Stage += 1
-	GameController._MiniGame.set_Timer(30.0)
+	GameController._MiniGame.set_Timer(2.0)
 	GameController._MiniGame.set_Dificult(1)
 	print(GameController.Stage)
 #	GameController.test += 1
 #	print(GameController.test)
 	get_tree().change_scene_to(minigame)
-
-func _ready():
-	GameController.Level = 0
